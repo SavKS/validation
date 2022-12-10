32ec693114b84afdbc504fc1ed21964e1f890cfe
Andrii Savluk <a.savluk@ideil.com>
2022-12-10 12:45:42 +0200

    wip

---
+++ src/Providers/ValidationServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Savks\Validation\Providers\ValidationServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class ValidationServiceProvider extends ServiceProvider
+{
+
+}
